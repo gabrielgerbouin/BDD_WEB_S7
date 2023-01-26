059908c1967e17d97795e7338b6662cf07615c4e
gabrielgerbouin <gabrielgerbouin53210@gmail.com>
2023-01-26 15:16:15 +0100

    Update Requete8.sql
    Ne renvoie pas encore le min et max

--- Requete8.sql
+++ Requete8.sql
@@ -1,2 +1,14 @@
 -- gerbouga
--- Le meilleur et le pire club avec la moyenne et le nombre total de dessins
+-- Le meilleur et le pire club avec la moyenne et le nombre total de leurs dessins
+-- ne renvoie pas encore le min et max
+SELECT Club.nomClub, AVG(Evaluation.note) AS moyenne, COUNT(Dessin.numDessin) AS nombreDeDessins
+FROM Club, Evaluation, Dessin, Concours, ParticipeClub
+WHERE Evaluation.numDessin = Dessin.numDessin
+AND Dessin.numConcours = Concours.numConcours
+AND Concours.numConcours = ParticipeClub.numConcours
+AND ParticipeClub.numClub = Club.numClub
+GROUP BY Club.nomClub
+ORDER BY AVG(Evaluation.note) ASC
+
+-- 9, 10, 11, 12 : max (10.13548951724974)
+-- 5,  6,  7,  8 : min (10.109337408709443)
