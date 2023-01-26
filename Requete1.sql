32a8c5780e76d3744b2c2f88deb2af60af9853dd
gabrielgerbouin <gabrielgerbouin53210@gmail.com>
2023-01-26 09:14:47 +0100

    Validation Requete1.sql

--- Requete1.sql
+++ Requete1.sql
@@ -1,5 +1,5 @@
 -- pommergu
--- testee, a valider ensemble
+-- testee, validee
 SELECT Utilisateur.nom, Utilisateur.adresse, Utilisateur.age, Concours.descriptif, Concours.datedeb, Concours.datefin, Club.nomClub as "Club", Club.departement, Club.region 
 FROM Utilisateur, Concours, Club, Competiteur, ParticipeCompetiteur, ParticipeClub 
 Where Utilisateur.numUtilisateur = Competiteur.numCompetiteur  
@@ -7,6 +7,6 @@ AND Competiteur.numCompetiteur = ParticipeCompetiteur.numCompetiteur -- On s ass
 AND ParticipeCompetiteur.numConcours = Concours.numConcours          -- On s assure qu'on sélectionne un concours de ce même utilisateur 
 AND ParticipeClub.numClub = Utilisateur.numClub      -- On sélectionne le numéro de club de l utilisateur  
 AND ParticipeClub.numConcours = Concours.numConcours  
-AND Utilisateur.numClub = Club.numClub      -- PROPOSITION DE CORRECTION GAB : 192 RESULTATS, PLUS DE 3000 AVANT 
+AND Utilisateur.numClub = Club.numClub      -- On lie la table Utilisateur et la table Club par leur clé numClub 
 -- Fin de la sélection, début des conditions 
 AND YEAR(Concours.datedeb) = 2021 -- YEAR(une_date) permet de retourner lannée uniquement
