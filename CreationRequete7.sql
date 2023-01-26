5c3aa14f32036a0be66f0e4820395bd3690ffb5a
gabrielgerbouin <gabrielgerbouin53210@gmail.com>
2023-01-26 13:26:12 +0100

    Validation requete7.sql

--- CreationRequete7.sql
+++ CreationRequete7.sql
@@ -6,5 +6,3 @@ nomEval         Varchar (60) NOT NULL,
 prenomEval      Varchar (60) NOT NULL, 
 moyenne         float check  (moyenne between 0 and 20),
 numConcours     smallint NOT NULL);
-
--- PRIMARY KEY (nomEval)) ;
--- Requete7.sql
+++ Requete7.sql
@@ -1,11 +1,16 @@
 -- gerbouga
--- En Cours
+-- testee, validee
+-- Premiere requete, pour selectionner le nom et prenom de chaque evaluateur, et la moyenne qu'il a donné pour tous les concours
 INSERT INTO TempTableRequete7
 SELECT Utilisateur.nom, Utilisateur.prenom, AVG(Evaluation.note) AS moyenne, Concours.numConcours
 FROM Utilisateur, Evaluation, Concours, Dessin, Evaluateur
-WHERE Evaluation.numDessin = Dessin.numDessin
-AND Dessin.numConcours = Concours.numConcours
-AND Evaluation.numEvaluateur = Evaluateur.numEvaluateur
-AND Evaluateur.numEvaluateur = Utilisateur.numUtilisateur
-GROUP BY Evaluation.numEvaluateur, Concours.numConcours
-ORDER BY AVG(Evaluation.note) ASC;
+WHERE Evaluation.numDessin = Dessin.numDessin -- On lie la table Evaluation et la table Dessin
+AND Dessin.numConcours = Concours.numConcours -- On lie la table Dessin et la table Concours
+AND Evaluation.numEvaluateur = Evaluateur.numEvaluateur -- On lie la table Evaluation et la table Evaluateur
+AND Evaluateur.numEvaluateur = Utilisateur.numUtilisateur -- On lie la table Evaluateur et la table Utilisateur
+GROUP BY Evaluation.numEvaluateur, Concours.numConcours -- On regroupe les résultats par rapport au numéro d'évaluateur et au numéro de concours
+ORDER BY AVG(Evaluation.note) ASC; -- On trie les résultats par note moyenne croissante
+-- Seconde requete, pour selectionner la moyenne minimale par concours
+SELECT nomEval, prenomEval, MIN(moyenne) AS moyenneMinimale, numConcours
+FROM TempTableRequete7
+GROUP BY numConcours;
