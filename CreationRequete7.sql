2b379f52e753e82041c31e5a7c36497940de1953
gabrielgerbouin <gabrielgerbouin53210@gmail.com>
2023-02-01 16:14:02 +0100

    Merge CreationRequete7.sql dans Requete7.sql

--- CreationRequete7.sql
+++
@@ -1,8 +0,0 @@
-﻿drop table if exists TempTableRequete7; 
-
-Create Table TempTableRequete7
-(    
-nomEval         Varchar (60) NOT NULL,
-prenomEval      Varchar (60) NOT NULL, 
-moyenne         float check  (moyenne between 0 and 20),
-numConcours     smallint NOT NULL);
--- Requete7.sql
+++ Requete7.sql
@@ -1,5 +1,17 @@
 -- gerbouga
+-- Suppression de la table TempTableRequete7 si elle n'a pas ete supprimee auparavant
+drop table if exists TempTableRequete7; 
+
+-- Creation de la table temporaire TempTableRequete7
+Create Table TempTableRequete7
+(    
+nomEval         Varchar (60) NOT NULL,
+prenomEval      Varchar (60) NOT NULL, 
+moyenne         float check  (moyenne between 0 and 20),
+numConcours     smallint NOT NULL);
+
 -- Premiere requete, pour selectionner le nom et prenom de chaque evaluateur, et la moyenne qu'il a donné pour tous les concours
+-- Requete inseree dans la table temporaire TempTableRequete7
 INSERT INTO TempTableRequete7
 SELECT Utilisateur.nom, Utilisateur.prenom, AVG(Evaluation.note) AS moyenne, Concours.numConcours
 FROM Utilisateur, Evaluation, Concours, Dessin, Evaluateur
@@ -9,7 +21,11 @@ AND Evaluation.numEvaluateur = Evaluateur.numEvaluateur -- On lie la table Evalu
 AND Evaluateur.numEvaluateur = Utilisateur.numUtilisateur -- On lie la table Evaluateur et la table Utilisateur
 GROUP BY Evaluation.numEvaluateur, Concours.numConcours -- On regroupe les résultats par rapport au numéro d'évaluateur et au numéro de concours
 ORDER BY AVG(Evaluation.note) ASC; -- On trie les résultats par note moyenne croissante
+
 -- Seconde requete, pour selectionner la moyenne minimale par concours
 SELECT nomEval, prenomEval, MIN(moyenne) AS moyenneMinimale, numConcours
 FROM TempTableRequete7
-GROUP BY numConcours;
+GROUP BY numConcours; -- On regroupe les résultats par rapport au numéro de concours
+
+-- Suppression de la table TempTableRequete7 en fin de requete
+drop table if exists TempTableRequete7;
